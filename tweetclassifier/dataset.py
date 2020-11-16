14a5d47d772d958606b6490b3b89ea96837fa676
Mohammed Khalilia <khallia@amazon.com>
2020-11-15 21:34:25 -0800

    Finished dataloaders
    * Added basic BERT sequence classifier
    
    * Dataloaders
    
    * Mapping tokens to indices

--- tweetclassifier/dataset.py
+++ tweetclassifier/dataset.py
@@ -1,40 +1,55 @@
 import torch
-from torchtext.data import Field, TabularDataset, BucketIterator
+import torchtext
 from transformers import BertTokenizer
 
 
 def get_dataloaders(input_path):
     tokenizer = BertTokenizer.from_pretrained("bert-base-uncased", do_lower_case=True)
-    tweet_field = Field(batch_first=True, tokenize=tokenizer.encode, use_vocab=False)
-    label_field = Field(batch_first=True, use_vocab=True, sequential=False)
 
-    fields = {
-        "tweet": ('tweet', tweet_field),
-        "label": ('label', label_field)
-    }
+    tweet_field = torchtext.data.Field(
+        batch_first=True, tokenize=tokenizer.encode, use_vocab=False, pad_token=0
+    )
 
-    train_data, val_data, test_data = TabularDataset.splits(path=input_path,
-                                                            train="train.json",
-                                                            validation="dev.json",
-                                                            test="test.json",
-                                                            fields=fields,
-                                                            format="json")
+    label_field = torchtext.data.LabelField(
+        batch_first=True,
+        use_vocab=True
+    )
 
-    train_dataloader = BucketIterator(train_data,
-                                      batch_size=32,
-                                      shuffle=True,
-                                      train=True,
-                                      device=torch.device("cuda:0"))
-    val_dataloader = BucketIterator(val_data,
-                                    batch_size=32,
-                                    shuffle=True,
-                                    train=True,
-                                    device=torch.device("cuda:0"))
-    test_dataloader = BucketIterator(test_data,
-                                     batch_size=32,
-                                     shuffle=False,
-                                     train=False,
-                                     device=torch.device("cuda:0"))
+    fields = {"tweet": ("tweet", tweet_field), "label": ("label", label_field)}
 
-    label_field.build_vocab(train_data)
+    train_data, val_data, test_data = torchtext.data.TabularDataset.splits(
+        path=input_path,
+        train="train.json",
+        validation="dev.json",
+        test="test.json",
+        fields=fields,
+        format="json",
+    )
+
+    train_dataloader = torchtext.data.BucketIterator(
+        train_data,
+        batch_size=32,
+        shuffle=True,
+        train=True,
+        device=torch.device("cuda:0"),
+    )
+
+    val_dataloader = torchtext.data.BucketIterator(
+        val_data,
+        batch_size=32,
+        shuffle=True,
+        train=True,
+        device=torch.device("cuda:0")
+    )
+
+    test_dataloader = torchtext.data.BucketIterator(
+        test_data,
+        batch_size=32,
+        shuffle=False,
+        train=False,
+        device=torch.device("cuda:0"),
+    )
+
+    label_field.build_vocab(train_data, val_data, test_data)
+
     return train_dataloader, val_dataloader, test_dataloader
--- tweetclassifier/trainer.py
+++ tweetclassifier/trainer.py
@@ -1,6 +1,24 @@
+import torch
+
+
 class Trainer:
-    def __init__(self):
-        pass
+    def __init__(
+        self,
+        model,
+        max_epochs=50,
+        train_dataloader=None,
+        val_dataloader=None,
+        test_dataloader=None,
+    ):
+        self.model = model
+        self.max_epochs = max_epochs
+        self.train_dataloader = train_dataloader
+        self.val_dataloader = val_dataloader
+        self.test_dataloader = test_dataloader
 
     def train(self):
-        pass
+        for i in range(self.max_epochs):
+            for tweets, labels in self.train_dataloader:
+                if torch.cuda.is_available():
+                    tweets = tweets.cuda()
+                    labels = labels.cuda()

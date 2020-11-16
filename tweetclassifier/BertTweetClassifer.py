d0359b8c18d3edbb44a423b48cad083cbda6cf4f
Mohammed Khalilia <khallia@amazon.com>
2020-11-15 21:34:25 -0800

    Finished dataloaders
    * Added basic BERT sequence classifier
    
    * Dataloaders
    
    * Mapping tokens to indices

--- tweetclassifier/BertTweetClassifer.py
+++ tweetclassifier/BertTweetClassifer.py
@@ -1,9 +1,13 @@
 import torch.nn as nn
+from transformers import BertForSequenceClassification
 
 
 class BertTweetClassifer(nn.Module):
-    def __init__(self):
+    def __init__(self, num_labels=2):
         super(BertTweetClassifer).__init__()
-
-    def forward(self):
-        pass
+        self.model = BertForSequenceClassification.from_pretrained(
+            "bert-base-uncased",
+            num_labels=num_labels,
+            output_attentions=False,
+            output_hidden_states=False,
+        )
--- tweetclassifier/bin/train.py
+++ tweetclassifier/bin/train.py
@@ -1,5 +1,7 @@
 import argparse
 from tweetclassifier.dataset import get_dataloaders
+from tweetclassifier.trainer import Trainer
+from transformers import BertForSequenceClassification
 
 
 def parse_args():
@@ -14,13 +16,35 @@ def parse_args():
         help="Path to training data",
     )
 
+    parser.add_argument(
+        "--max_epochs",
+        type=int,
+        default=50,
+        help="Number of epochs",
+    )
+
     args = parser.parse_args()
 
     return args
 
 
 def main(args):
-    get_dataloaders(input_path=args.input_path)
+    model = BertForSequenceClassification.from_pretrained(
+        "bert-base-uncased",
+        num_labels=2,
+        output_attentions=False,
+        output_hidden_states=False,
+    )
+
+    train_dataloader, val_dataloader, test_dataloader = get_dataloaders(input_path=args.input_path)
+
+    trainer = Trainer(model=model,
+                      max_epochs=args.max_epochs,
+                      train_dataloader=train_dataloader,
+                      val_dataloader=val_dataloader,
+                      test_dataloader=test_dataloader)
+    trainer.train()
+    return
 
 
 if __name__ == "__main__":

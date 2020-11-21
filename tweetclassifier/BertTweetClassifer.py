23c9b6fb4ea511deebf28c34bed892cd9366b108
Mohammed Khalilia <khallia@amazon.com>
2020-11-20 22:21:47 -0800

    Working trainer
    * Classifier
    
    * Evaluate on val/test data
    
    * Problem with loss function

--- tweetclassifier/BertTweetClassifer.py
+++ tweetclassifier/BertTweetClassifer.py
@@ -1,13 +1,17 @@
 import torch.nn as nn
-from transformers import BertForSequenceClassification
+from transformers import BertModel, BertPreTrainedModel
 
 
 class BertTweetClassifer(nn.Module):
-    def __init__(self, num_labels=2):
-        super(BertTweetClassifer).__init__()
-        self.model = BertForSequenceClassification.from_pretrained(
-            "bert-base-uncased",
-            num_labels=num_labels,
-            output_attentions=False,
-            output_hidden_states=False,
-        )
+    def __init__(self, bert_model, num_labels=2):
+        super().__init__()
+
+        self.encoder = BertModel.from_pretrained(bert_model)
+        self.dropout = nn.Dropout(0.2)
+        self.classifier = nn.Linear(768, num_labels)
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.dropout(x[1])
+        y = self.classifier(x)
+        return y
---
+++ tweetclassifier/bin/csv2json.py
@@ -0,0 +1,37 @@
+import csv
+import json
+import random
+import numpy as np
+import os
+
+csv_file = "/mnt/efs/data/twitter_us_airline_sentiment/twitter_us_airline_sentiment.csv"
+output_path = "/mnt/efs/data/twitter_us_airline_sentiment/"
+tweets = list()
+train_ratio = 0.7
+dev_ratio = 0.1
+
+with open(csv_file, "r") as fh:
+    rows = csv.reader(fh)
+    next(rows)
+
+    for row in rows:
+        tweets.append({
+            "tweet": row[10],
+            "labels": [row[1]]
+        })
+
+random.shuffle(tweets)
+n = len(tweets)
+ratios = [int(train_ratio * n), int((train_ratio + dev_ratio) * n)]
+train_tweets, val_tweets, test_tweets = np.split(tweets, ratios)
+
+train_file = os.path.join(output_path, "train.json")
+dev_file = os.path.join(output_path, "val.json")
+test_file = os.path.join(output_path, "test.json")
+
+with open(train_file, "w") as fh:
+    json.dump(list(train_tweets), fh, indent=4)
+with open(dev_file, "w") as fh:
+    json.dump(list(val_tweets), fh, indent=4)
+with open(test_file, "w") as fh:
+    json.dump(list(test_tweets), fh, indent=4)

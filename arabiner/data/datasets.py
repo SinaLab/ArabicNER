b5d4135b78ca60dccafc0d31c0b558d0967001c3
Mohammed Khalilia <khallia@amazon.com>
2021-12-24 00:29:55 -0800

    Dataset and Data Transformer for Leveled Tags
    * Map tags to multiple levels.
    * Number of levels for a segment is the depth of the nested entities.

--- arabiner/data/datasets.py
+++ arabiner/data/datasets.py
@@ -1,6 +1,6 @@
 from torch.utils.data import Dataset
 from torch.nn.utils.rnn import pad_sequence
-from arabiner.data.transforms import BertSeqTransform, BertSeqMultiLabelTransform
+from arabiner.data.transforms import BertSeqTransform, BertSeqMultiLabelTransform, BertSeqLayeredTransform
 import logging
 
 logger = logging.getLogger(__name__)
@@ -20,7 +20,7 @@ class Token:
 
     def __str__(self):
         """
-        Token text represenation
+        Token text representation
         :return: str
         """
         gold_tags = "|".join(self.gold_tag)
@@ -120,4 +120,49 @@ class MultiLabelDataset(Dataset):
         subwords = pad_sequence(subwords, batch_first=True, padding_value=0)
         tags = pad_sequence(tags, batch_first=True, padding_value=0)
 
+        return subwords, tags, tokens, valid_len
+
+
+class LayeredLabelDataset(Dataset):
+    def __init__(
+        self,
+        examples=None,
+        vocab=None,
+        bert_model="aubmindlab/bert-base-arabertv2",
+        max_seq_len=512
+    ):
+        """
+        The dataset that used to transform the segments into training data
+        :param examples: list[[tuple]] - [[(token, tag), (token, tag), ...], [(token, tag), ...]]
+                         You can get generate examples from -- arabiner.data.dataset.parse_conll_files
+        :param vocab: vocab object containing indexed tags and tokens
+        :param bert_model: str - BERT model
+        :param: int - maximum sequence length
+        """
+        self.transform = BertSeqLayeredTransform(bert_model, vocab, max_seq_len=max_seq_len)
+        self.examples = examples
+        self.vocab = vocab
+
+    def __len__(self):
+        return len(self.examples)
+
+    def __getitem__(self, item):
+        subwords, tags, tokens, valid_len = self.transform(self.examples[item])
+        return subwords, tags, tokens, valid_len
+
+    def collate_fn(self, batch):
+        """
+        Collate function that is called when the batch is called by the trainer
+        :param batch: Dataloader batch
+        :return: Same output as the __getitem__ function
+        """
+        subwords, tags, tokens, valid_len = zip(*batch)
+
+        # Pad sequences in this batch
+        # subwords and tokens are padded with zeros
+        # tags are padding with the index of the O tag
+        subwords = pad_sequence(subwords, batch_first=True, padding_value=0)
+        tags = pad_sequence(
+            tags, batch_first=True, padding_value=self.vocab.tags.stoi["O"]
+        )
         return subwords, tags, tokens, valid_len
--- arabiner/data/transforms.py
+++ arabiner/data/transforms.py
@@ -2,6 +2,7 @@ import torch
 from transformers import BertTokenizer
 from functools import partial
 import logging
+import itertools
 import arabiner
 
 logger = logging.getLogger(__name__)
@@ -39,8 +40,8 @@ class BertSeqTransform:
         subwords.insert(0, self.tokenizer.cls_token_id)
         subwords.append(self.tokenizer.sep_token_id)
 
-        tags.insert(0, self.vocab.tags.stoi["<start>"])
-        tags.append(self.vocab.tags.stoi["<stop>"])
+        tags.insert(0, self.vocab.tags.stoi["O"])
+        tags.append(self.vocab.tags.stoi["O"])
 
         tokens.insert(0, unk_token)
         tokens.append(unk_token)
@@ -99,3 +100,67 @@ class BertSeqMultiLabelTransform:
         tokens.append(unk_token)
 
         return torch.LongTensor(subwords), torch.stack(tags).squeeze(1), tokens, len(tokens)
+
+
+class BertSeqLayeredTransform:
+    def __init__(self, bert_model, vocab, max_seq_len=512):
+        self.tokenizer = BertTokenizer.from_pretrained(bert_model)
+        self.encoder = partial(
+            self.tokenizer.encode,
+            max_length=max_seq_len,
+            truncation=True,
+        )
+        self.max_seq_len = max_seq_len
+        self.vocab = vocab
+
+    def __call__(self, segment):
+        tags, tokens, token2subwords = list(), list(), list()
+        unk_token = arabiner.data.datasets.Token(text="UNK")
+
+        # Number of nested tag levels is defined by the token with most tags
+        num_levels = max([len(token.gold_tag) for token in segment])
+
+        for token in segment:
+            token2subwords.append(self.encoder(token.text)[1:-1])
+
+            # Add UNK token for all subwords except for the first subword
+            tokens += [token] + [unk_token] * (len(token2subwords[-1]) - 1)
+
+        subwords = list(itertools.chain(*token2subwords))
+
+        for l in range(num_levels):
+            level_tags = list()
+            for t, token in enumerate(segment):
+                # Get the tags for this token at al levels, some levels do not exist for this token, in
+                # such case we will use the "O" tag
+                token_tags = token.gold_tag + ["O"] * (num_levels - len(token.gold_tag))
+
+                # convert tags to indices and add "O" to all subwords except for the the first subword
+                level_tags += [self.vocab.tags.stoi[token_tags[l]]] + [self.vocab.tags.stoi["O"]] * (len(token2subwords[t]) - 1)
+
+            tags.append(level_tags)
+
+        # Truncate to max_seq_len
+        if len(subwords) > self.max_seq_len - 2:
+            text = " ".join([t.text for t in tokens if t.text != "UNK"])
+            logger.info("Truncating the sequence %s to %d", text, self.max_seq_len - 2)
+            subwords = subwords[:self.max_seq_len - 2]
+            tags = [t[:self.max_seq_len - 2] for t in tags]
+            tokens = tokens[:self.max_seq_len - 2]
+
+        # Add "O" tags for the first and last subwords
+        tags = torch.Tensor(tags)
+        tags = torch.column_stack((
+            torch.full((num_levels,), self.vocab.tags.stoi["O"]),
+            tags,
+            torch.full((num_levels,), self.vocab.tags.stoi["O"]),
+        ))
+
+        tokens.insert(0, unk_token)
+        tokens.append(unk_token)
+
+        subwords.insert(0, self.tokenizer.cls_token_id)
+        subwords.append(self.tokenizer.sep_token_id)
+        subwords = torch.LongTensor(subwords)
+
+        return subwords, tags, tokens, len(tokens)

b18ee2b6fab6e180f66a9cf8226ba7fe05b8d971
Mohammed Khalilia <khallia@amazon.com>
2021-12-28 20:22:07 -0800

    None Overlapping SameType Transform and Dataset
    * We will create a separate layer for each entity type

--- arabiner/data/transforms.py
+++ arabiner/data/transforms.py
@@ -2,6 +2,7 @@ import torch
 from transformers import BertTokenizer
 from functools import partial
 import logging
+import re
 import itertools
 import arabiner
 
@@ -164,3 +165,71 @@ class BertSeqLayeredTransform:
         subwords = torch.LongTensor(subwords)
 
         return subwords, tags, tokens, len(tokens)
+
+
+class NoneOverlappingSameTypeTransform:
+    def __init__(self, bert_model, vocab, max_seq_len=512):
+        self.tokenizer = BertTokenizer.from_pretrained(bert_model)
+        self.encoder = partial(
+            self.tokenizer.encode,
+            max_length=max_seq_len,
+            truncation=True,
+        )
+        self.max_seq_len = max_seq_len
+        self.vocab = vocab
+        self.tag_names = list(set([tag.split("-", 1)[1] for tag in vocab.tags.itos if "-" in tag]))
+
+    def __call__(self, segment):
+        tags, tokens, subwords = list(), list(), list()
+        unk_token = arabiner.data.datasets.Token(text="UNK")
+
+        # Encode each token and get its subwords and IDs
+        for token in segment:
+            token.subwords = self.encoder(token.text)[1:-1]
+            subwords += token.subwords
+            tokens += [token] + [unk_token] * (len(token.subwords ) - 1)
+
+        # Construct the labels for each tag type
+        # The sequence will have a list of tags for each type
+        # The final tags for a sequence is a matrix NUM_TAG_TYPES x SEQ_LEN
+        # Example:
+        #   [
+        #       [O, O, B-PERS, I-PERS, O, O, O]
+        #       [B-ORG, I-ORG, O, O, O, O, O]
+        #       [O, B-GPE, O, O, O, O, O]
+        #   ]
+        for tag_name in self.tag_names:
+            r = re.compile(".*-" + tag_name)
+            single_type_tags = [(list(filter(r.match, token.gold_tag))
+                                or ["O"]) + ["O"] * (len(token.subwords) - 1)
+                                for token in segment]
+            single_type_tags = list(itertools.chain(*single_type_tags))
+            tags.append([self.vocab.tags.stoi[tag] for tag in single_type_tags])
+
+        # Truncate to max_seq_len
+        if len(subwords) > self.max_seq_len - 2:
+            text = " ".join([t.text for t in tokens if t.text != "UNK"])
+            logger.info("Truncating the sequence %s to %d", text, self.max_seq_len - 2)
+            subwords = subwords[:self.max_seq_len - 2]
+            tags = [t[:self.max_seq_len - 2] for t in tags]
+            tokens = tokens[:self.max_seq_len - 2]
+
+        # Add dummy token at the start end of sequence
+        tokens.insert(0, unk_token)
+        tokens.append(unk_token)
+
+        # Add CLS and SEP at start end of subwords
+        subwords.insert(0, self.tokenizer.cls_token_id)
+        subwords.append(self.tokenizer.sep_token_id)
+        subwords = torch.LongTensor(subwords)
+
+        # Add "O" tags for the first and last subwords
+        tags = torch.Tensor(tags)
+        tags = torch.column_stack((
+            torch.full((len(self.tag_names),), self.vocab.tags.stoi["O"]),
+            tags,
+            torch.full((len(self.tag_names),), self.vocab.tags.stoi["O"]),
+        ))
+
+        return subwords, tags, tokens, len(tokens)
+
